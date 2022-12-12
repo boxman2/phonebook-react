f267325bd7de25d87a75e56822839fbd5b8c8474
boxman2 <boxman2@naver.com>
2022-12-12 17:47:56 +0900

    폰북 틀 완료

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
+import ContactList from "./components/ContactList";
+import ContactForm from "./components/ContactForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <h1 className="center">연락처</h1>
+        <Row>
+          <Col>
+            <ContactForm />
+          </Col>
+          <Col>
+            <ContactList />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
-
 export default App;
